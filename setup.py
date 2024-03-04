71b534161ac860aa432de80e4f44278c0f65f763
mam <ma.martinod@free.fr>
2024-03-04 15:56:12 +0100

    Release of a new version with correction of mistakes in the optimizers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grip',
-    version='1.0.0',
+    version='1.1.0',
     author='M.-A. Martinod',
     description='Self-calibration data reduction tools for nulling interferometry',
     packages=find_packages(),
