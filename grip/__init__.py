587d79985c4ce9612a4eba40fd8894e8cc8e7620
mam <ma.martinod@free.fr>
2024-05-08 12:24:43 +0200

    Correct import GRIP modules

--- grip/__init__.py
+++ grip/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "1.3.0"
 
 from grip.histogram_tools import *
-from grip.fit_tools import *
+from grip.fitting import *
 from grip.preprocessing import *
 from grip.plots import *
 from grip.generic import *
 from grip.instrument_models import *
+from grip.load_files import *
